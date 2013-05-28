a2044ee38f5e8569e2afa01ac72ea1e8b0dd0468
Jiro Sas <zirosas@gmail.com>
2013-05-29 11:09:19 +1200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hitokoto::Application.config.secret_token = '1dac978a791c4253d54ca4ca49cd5dd6d26572ffc6db487dbef9938b50cfa92df33ff1e9df81b9cf404381dd76a4ef95c3997ff03b03308da262955ee35d3a38'
